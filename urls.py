c12569158d28c42cc2605dfd73997157d7f66f8c
Indira Bekturganova <119610125+wowlrd@users.noreply.github.com>
2023-05-15 09:48:53 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,28 +1,29 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
-
-    path('products/<int:pk>/', views.product_detail, name='product_detail'),
-    path('site_admin/', views.site_admin, name='site_admin'),
-    path('profile/', views.profile, name='profile'),
-
-    path('basket', views.MyBasket, name='basket'),
-    path('add-to-basket/<int:pk>', views.addToBasket, name='add-to-basket'),
-    path('remove-item/<int:item_id>/', views.remove_item, name='remove_item'),
-
-    path('sign_in/', views.sign_in, name='sign_in'),
-    path('sign_up/', views.sign_up, name='sign_up'),
-    path('logout', views.log_out, name='logout'),
-
-    path('profile/edit_profile/', views.edit_profile, name='edit_profile'),
-    path('site_admin/user_list', views.user_list, name='user_list'),
-    path('site_admin/create_user', views.create_user, name='create_user'),
-    path('site_admin/edit_user/<str:username>', views.edit_user, name='edit_user'),
-    path('site_admin/delete_user/<str:username>', views.delete_user, name='delete_user'),
-
-    path('product-search/', views.product_search, name='product_search'),
-    path('filtered_catalog/', views.filtered_catalog, name='filtered_catalog')
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls'), name='myapp')
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
